d72897488f8e898ba2297b95af6527715242948c
Lucas Vicente <lucasdev08@hotmail.com>
2025-06-04 17:25:20 -0300

    feat: web blog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { FeatureSection } from "@/components/feature-section/feature-section";
+import { Hero } from "@/components/hero-section/hero-section";
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-    </div>
+    <>
+      <article className="flex flex-col">
+        <Hero />
+        <FeatureSection />
+      </article>
+    </>
   );
 }
