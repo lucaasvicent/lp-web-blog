212fcce2207c06b80c9487225660f40cbbf87fd1
Lucas Vicente <lucasdev08@hotmail.com>
2025-07-10 23:27:20 -0300

    feat: Customer Story

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { CustomerStorySection } from "@/components/customer-story-section/customer-story-section";
 import { FeatureSection } from "@/components/feature-section/feature-section";
 import { Hero } from "@/components/hero-section/hero-section";
+import { SupportSection } from "@/components/support-section/support-section";
 
 export default function Home() {
   return (
@@ -7,6 +9,8 @@ export default function Home() {
       <article className="flex flex-col">
         <Hero />
         <FeatureSection />
+        <SupportSection />
+        <CustomerStorySection />
       </article>
     </>
   );
