3227cecee40d9cc836a86b76a3591a01c7b12780
Lucas Vicente <lucasdev08@hotmail.com>
2025-08-19 16:06:06 -0300

    feat: add seo in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
 import { LandingPage } from "@/templates/landing-page/landing-page";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Site.set',
+  description: 'Venda seus produtos como afiliado em um único lugar!',
+  robots: 'index,follow',
+  openGraph: {
+    title: 'Site.set',
+    description: 'Venda seus produtos como afiliado em um único lugar!',
+    url: 'https://lp-web-blog.vercel.app/og-image.jpg',
+    siteName: 'Site.set',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://lp-web-blog.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.set',
+      },
+    ],
+  }
+}
+
 export default function HomePage() {
   return (
     <LandingPage />
